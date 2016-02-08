4e0d6503c4295be2255842011681bac34ae68817
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-08 20:15:24 +0100

    Adjusted the Tabs and Spaces

--- Dice.py
+++ Dice.py
@@ -5,18 +5,19 @@
 from random import randrange
 
 def dice():
-        a = randrange(1,7)
-        if a == 1:
-                print ("1")
-        if a == 2:
-                print ("2")
-        if a == 3:
-                print ("3")
-        if a == 4:
-                print ("4")
-        if a == 5:
-                print ("5")
-        if a == 6:
-                print (" 6 - BINGO!!!")
-                print (15 * "*")
+    a = randrange(1,7)
+    if a == 1:
+        print ("1")
+    if a == 2:
+        print ("2")
+    if a == 3:
+        print ("3")
+    if a == 4:
+        print ("4")
+    if a == 5:
+        print ("5")
+    if a == 6:
+        print (" 6 - BINGO!!!")
+        print (15 * "*")
+dice()
     
