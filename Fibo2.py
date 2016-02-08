6b448bd129adc974ac1222cdc9e6a4a81cea772d
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-08 20:19:46 +0100

    Adjusted the Tabs and Spaces

--- Fibo2.py
+++ Fibo2.py
@@ -3,17 +3,17 @@
 # Thema: Fobonacci von einer Zahl errechnen
 
 def fibo2(n):
-        if n < 0:
-                return "Negative Zahlen sind nicht erlaubt!!!"
-        if (n == 1):
-                return 1
-        if (n == 0):
-                return 0
-        a = 0
-        b = 1
-        for i in range (2, n + 1):
-                s = a + b
-                a = b
-                b = s
-        return b
+    if n < 0:
+        return "Negative Zahlen sind nicht erlaubt!!!"
+    if (n == 1):
+        return 1
+    if (n == 0):
+        return 0
+    a = 0
+    b = 1
+    for i in range (2, n + 1):
+        s = a + b
+        a = b
+        b = s
+    return b
     
