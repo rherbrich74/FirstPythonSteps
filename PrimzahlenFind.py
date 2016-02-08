097f1ff37c79e3e2be9bc24ff4a95ea4b6970ba3
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-08 20:27:28 +0100

    Adjusted the Tabs and Spaces

--- PrimzahlenFind.py
+++ PrimzahlenFind.py
@@ -3,24 +3,19 @@
 # Thema: Listen von Primzahlen
 
 def is_pz (n):
-     k = 0 ; m = (n//2 ) + 1
-     if ((n == 0) or (n == 1)):
-          return False
-     if n < 0:
-          return False
-     for k in range (2, m):
-          if (n % k) == 0:
-               return False
-     return True
+    k = 0 ; m = (n//2 ) + 1
+    if ((n == 0) or (n == 1)):
+        return False
+    if n < 0:
+        return False
+    for k in range (2, m):
+        if (n % k) == 0:
+            return False
+    return True
 
 def find_pz (n):  
-	PrimZ = [] ; m = n + 1
-	for i in range (2, m):
-		if is_pz (i):
-			PrimZ += [i]
-	return PrimZ
-    
-    
-    
-    
-    
+    PrimZ = [] ; m = n + 1
+    for i in range (2, m):
+        if is_pz (i):
+            PrimZ += [i]
+    return PrimZ
