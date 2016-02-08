40766dc4aea6d263e44fb6148d6dd09aef3d46d4
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-08 20:19:09 +0100

    Adjusted the Tabs and Spaces

--- Fibo.py
+++ Fibo.py
@@ -3,10 +3,10 @@
 # Thema: Fibonacci von Zahlen errechnen
 
 def fibo(n):
-        if n < 0:
-                return "Negative Zahlen sind nicht erlaubt!!!"
-        if (n == 1):
-                return 1
-        if (n == 0):
-                return 0
-        return fibo(n - 1) + fibo(n - 2)
+    if n < 0:
+        return "Negative Zahlen sind nicht erlaubt!!!"
+    if (n == 1):
+        return 1
+    if (n == 0):
+        return 0
+    return fibo(n - 1) + fibo(n - 2)
