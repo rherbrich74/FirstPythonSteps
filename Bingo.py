200e7c8153d9230f59379e77c74a8369958d6904
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-08 20:03:08 +0100

    Adjusted the Tabs and Spaces

--- Bingo.py
+++ Bingo.py
@@ -5,13 +5,13 @@
 from random import *
 
 def bingo():
-        a = randrange(1,26)
-        b = randrange(1,26)
-        if (a == b):
-                print (a,b,"Herzlichen Glueckwunsch. Sie haben den Bingo geknackt")
-                print (55 * "*")
-        else:
-                print (a,b,"Schade :( Naechstes mal hast du vielleicht mehr Glueck")
+    a = randrange(1,26)
+    b = randrange(1,26)
+    if (a == b):
+        print (a,b,"Herzlichen Glueckwunsch. Sie haben den Bingo geknackt")
+        print (55 * "*")
+    else:
+        print (a,b,"Schade :( Naechstes mal hast du vielleicht mehr Glueck")
     
         
     
