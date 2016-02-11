ebcdef34a3ecd21c47e3a05b3fab5f1c8501d3fb
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-11 17:09:42 +0100

    Deleted

--- Delay.py
+++
@@ -1,9 +0,0 @@
-def delay_print(s, delay=0.05):
-    for c in str(s):
-        sys.stdout.write('%s' % c )
-        sys.stdout.flush()
-        time.sleep(delay)
-
-def delay_input(s):
-    delay_print(s)
-    return input()
