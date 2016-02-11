97e9f0ff62e6cce0ebf6985003cd563ac2550e8e
phy234 <alex@Ralfs-Mac-mini.local>
2016-02-11 18:10:17 +0100

    Last changes

--- WWM.py
+++ WWM.py
@@ -87,16 +87,16 @@ Gebe bitte die gewuenschte Schwierigkeitsstufe ein... \n""")
         time.sleep(wait_time * 4)
         Antwort = delay_input("Wie heisst die Hauptstadt von " + question + "?\n")
         if (Antwort.lower() == "joker"):
-            print (anwser)
+            print ("\n" + anwser)
             joker -= 1
-        if (Antwort.lower() == anwser.lower()):
-            #delay_print ("\nYeah! Sie haben die " + str(versuch) + ". Frage richtig beantwortet!\n\n")
-            delay_print ("\nJaa!!! Das war richtig!\n")
-            time.sleep(wait_time * 3)
-        else:
-            #delay_print ("\nOhh! Leider haben sie die " + str(versuch) + ". Frage falsch beantwortet!\n\n")
-            delay_print("\nOhh!!! Du lagst NICHT richtig! Die richtige Loesung waere " + anwser.upper() + " gewesen.\n")
-            time.sleep(wait_time * 4)
-            leben -= 1
-            benutzt.pop()
+            if (Antwort.lower() == anwser.lower()):
+                #delay_print ("\nYeah! Sie haben die " + str(versuch) + ". Frage richtig beantwortet!\n\n")
+                delay_print ("\nJaa!!! Das war richtig!\n")
+                time.sleep(wait_time * 3)
+            else:
+                #delay_print ("\nOhh! Leider haben sie die " + str(versuch) + ". Frage falsch beantwortet!\n\n")
+                delay_print("\nOhh!!! Du lagst NICHT richtig! Die richtige Loesung waere " + anwser.upper() + " gewesen.\n")
+                time.sleep(wait_time * 4)
+                leben -= 1
+                benutzt.pop()
 WWM()
